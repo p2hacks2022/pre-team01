975fbcbc4e84fc6babaa77a8c26806ba75e70e69
takumamiraidai <b1021236@fun.ac.jp>
2022-04-27 18:06:26 +0900

    prepare2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,17 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +32,10 @@ class MainActivity : ComponentActivity() {
                     Column {
                         Greeting("Takuma")
                         Message("Takuma", "Hello")
+                        MyButton()
+                        Counter()
+                        Toggle()
+                        Input()
                     }
                 }
             }
@@ -63,4 +68,54 @@ fun Message(username: String, body: String) {
         }
     }
     Text(text = body)
+}
+
+@Composable
+fun MyButton(){
+    Button(
+        onClick = { println("Hello, World")}
+    ){
+        Text(text = "Click")
+    }
+}
+
+@Composable
+fun Counter(){
+    var count by remember { mutableStateOf(0) }
+    Column{
+        Text(text = "Count: $count")
+        Button(
+            onClick = { count++}
+        ){
+            Text(text = "Click")
+        }
+    }
+}
+
+@Composable
+fun Toggle(){
+    var showText by remember { mutableStateOf(true) }
+    Column{
+        Button(onClick = { showText = !showText}) {
+            Text(text = "Click")
+        }
+        if (showText){
+            Text(text = "Hello")
+        }
+    }
+}
+
+@Composable
+fun Input(){
+    var input by remember { mutableStateOf("") }
+    Column{
+        TextField(
+            value = input,
+            onValueChange = { input =it }
+        )
+        Button(onClick = {
+            input = ""
+        }) { Text(text = "Send")}
+
+    }
 }
