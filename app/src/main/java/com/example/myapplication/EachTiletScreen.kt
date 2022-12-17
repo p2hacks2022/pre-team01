7963e25f6b35b5656784f3929d14c31bf7e329cb
takumamiraidai <b1021236@fun.ac.jp>
2022-12-17 16:26:05 +0900

    add-toiletpic

--- app/src/main/java/com/example/myapplication/EachTiletScreen.kt
+++ app/src/main/java/com/example/myapplication/EachTiletScreen.kt
@@ -1,24 +1,34 @@
 package com.example.myapplication
 
+import ScrollBoxes
+import ScrollBoxes2
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.IconButtonSample_5F
+import kotlinx.coroutines.NonCancellable.children
 
 @Composable
 fun ScreenManToilet1(toNextScreen: ()->Unit = {}) {
     val tint1 = animateColorAsState(
-        Color(0xFFEC407A)
+        Color(0xFFFFEB3B)
     )
     val tint2 = animateColorAsState(
         Color(0xFFB0BEC5)
@@ -49,9 +59,19 @@ fun ScreenManToilet1(toNextScreen: ()->Unit = {}) {
         }
 
         Text(text = "備考")
-        Text(text = "女子トイレと男子トイレ間違えやすい")
+        Text(text = "女子トイレと男子トイレ間違いやすい")
 
         Text(text = "空き状況")
+        IconButtonSample_5F(R.drawable.large_urinal, "l_icon") {}
+        IconButtonSample_5F(R.drawable.large_urinal_no1, "l_button") {}
+        IconButtonSample_5F(R.drawable.large_urinal_no2, "l_button") {}
+        IconButtonSample_5F(R.drawable.large_urinal_no3, "l_button") {}
+        IconButtonSample_5F(R.drawable.urinal, "s_icon") {}
+        IconButtonSample_5F(R.drawable.urinal_no1, "s_button") {}
+        IconButtonSample_5F(R.drawable.urinal_no2, "s_button") {}
+        IconButtonSample_5F(R.drawable.urinal_no3, "s_button") {}
+        IconButtonSample_5F(R.drawable.urinal_no4, "s_button") {}
+        IconButtonSample_5F(R.drawable.urinal_no5, "s_buttona") {}
         Text(text = "小")
         Text(text = "大")
 
@@ -72,13 +92,14 @@ fun ScreenManToilet1(toNextScreen: ()->Unit = {}) {
             IconToggleButtonSample()
             IconToggleButtonSample()
         }
+        Text(text = "星をタップしてください")
+
         Text(text = "レビューを投稿できます")
         Input()
 
-        Text(text = "レビュー")
-        Text(text = "レビュー")
-        Text(text = "レビュー")
-        Text(text = "レビュー")
+        Text(text = "みんなのレビュー")
+
+        ScrollBoxes2()
 
         Button(onClick = { toNextScreen() }) {
             Text(text = "Back")
