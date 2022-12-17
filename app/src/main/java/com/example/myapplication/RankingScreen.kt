8e0c9f9c19ad4fc56ebe5f04a8bad26db308dea4
kei-1111 <b1021116@fun.ac.jp>
2022-12-17 18:07:12 +0900

    MakeScreenRank

--- app/src/main/java/com/example/myapplication/RankingScreen.kt
+++ app/src/main/java/com/example/myapplication/RankingScreen.kt
@@ -1,8 +1,7 @@
 package com.example.myapplication
 
+import android.graphics.fonts.FontFamily
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -10,9 +9,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
@@ -26,7 +22,17 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.border
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.sp
+
+val titlesize = 120
+val subtitlesize = 25
+val textsize = 15
+val iconsize =30
+val photosize =140
+val space = 25
 
 @Preview
 @Composable
@@ -36,54 +42,47 @@ fun ScreenRank(toNextScreen: () -> Unit = {}) {
         verticalArrangement = Arrangement.Top
             )
     {
-        Row(
-            //modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Image(
-                painter = painterResource(R.drawable.crown),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(80.dp)
-                    .padding(
-                        top = 30.dp,
-                        start = 30.dp
-                    )
-            )
+        Image(
+            painter = painterResource(R.drawable.toileranking),
+            contentDescription = null,
+            modifier = Modifier
+                .size(410.dp,titlesize.dp)
+                .padding(
+                    top = space.dp,
+                    bottom = space.dp
+                )
+        )
 
-            Text(
-                text = "トイレランキング",
-                modifier = Modifier.padding(
-                    top = 30.dp,
-                    end = 30.dp
-                ),
-                fontSize = 35.sp
-            )
+        Row(
+            modifier = Modifier.size(410.dp,(subtitlesize + space).dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Mantoilet()
+            Womantoilet()
 
         }
         Row(
-            //modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.Center
+            modifier = Modifier.size(410.dp,(textsize + photosize + space).dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Text(
-                text = "男子トイレ",
-                modifier = Modifier.padding(
-                    top = 30.dp,
-                    start = 30.dp
-                ),
-                fontSize = 35.sp
-            )
-
-            Text(
-                text = "女子トイレ",
-                modifier = Modifier.padding(
-                    top = 30.dp,
-                    end = 30.dp
-                ),
-                fontSize = 35.sp
-            )
-
+            Rank(R.drawable.gold_crown, name = "5階トイレ", R.drawable.crown, "5F")
+            Rank(R.drawable.gold_crown, name = "5階トイレ", R.drawable.crown, "5F")
         }
+        Row(
+            modifier = Modifier.size(410.dp,(textsize + photosize + space).dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Rank(R.drawable.silver_crown, name = "5階トイレ", R.drawable.crown, "5F")
+            Rank(R.drawable.silver_crown, name = "5階トイレ", R.drawable.crown, "5F")
+        }
+        Row(
+            modifier = Modifier.size(410.dp,(textsize + photosize + space).dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Rank(R.drawable.bronze_crown, name = "5階トイレ", R.drawable.crown, "5F")
+            Rank(R.drawable.bronze_crown, name = "5階トイレ", R.drawable.crown, "5F")
+        }
+
     }
 
     Column(
@@ -96,10 +95,68 @@ fun ScreenRank(toNextScreen: () -> Unit = {}) {
     }
 }
 
+@Composable
+fun Rank(crownid: Int, name: String, fileid: Int, filedescription: String) {
+    Column(){
+        Row{
+            Image(
+                painter = painterResource(crownid),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(iconsize.dp)
+            )
+            Column(
+                modifier = Modifier.size(120.dp,iconsize.dp),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Text(
+                    text = " $name",
+                    fontSize = textsize.sp
+                )
+            }
+        }
+        Image(
+            painter = painterResource(fileid),
+            contentDescription = filedescription,
+            modifier = Modifier
+                .size(photosize.dp)
+        )
+    }
+}
 
 @Composable
-fun Rank() {
-
+fun Mantoilet() {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.man),
+            contentDescription = null,
+            modifier = Modifier
+                .size(iconsize.dp)
+        )
+        Text(
+            text = "男子トイレ",
+            fontSize = subtitlesize.sp
+        )
+    }
 }
+
+@Composable
+fun Womantoilet() {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.woman),
+            contentDescription = null,
+            modifier = Modifier
+                .size(iconsize.dp)
+        )
+        Text(
+            text = "女子トイレ",
+            fontSize = subtitlesize.sp
+        )
+    }
+}
+
+
+
 
 
